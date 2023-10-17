8a8984b907e96f2cc94348cdc996402a98e1880d
stevendegreat <stevendenjuguna@gmail.com>
2023-10-17 09:49:33 -0700

    simple shell

--- shell_loop.c
+++ shell_loop.c
@@ -126,8 +126,7 @@ fork_cmd(info);
 }
 else
 {
-if ((interactive(info) || _getenv(info, "PATH=")
-info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
+if ((interactive(info) || _getenv(info, "PATH=")) && info->argv[0][0] == '/' && is_cmd(info, info->argv[0]))
 fork_cmd(info);
 else if (*(info->arg) != '\n')
 {
